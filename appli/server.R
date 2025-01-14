ad69c83fcccb699a9c3f775f0370584c28b130b1
Jeanne MEDENOU <jeannemedenou@gmail.com>
2025-01-14 19:11:21 +0100

    Code final

---
+++ appli/server.R
@@ -0,0 +1,37 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+#Compilation des exercices
+fluidPage(
+  
+  # Application title
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar with a text input and slider inputs
+  sidebarLayout(
+    sidebarPanel(
+      textInput("nom", "Quel est ton nom ?"),
+      sliderInput("curseur", label = "Si x est ", min = 1, max = 100, value = 30), 
+      sliderInput("curseur2", label = "Si y est ", min = 1, max = 100, value = 5), 
+      "alors x * y est : ",
+      selectInput("dataset", "Dataset", choices = c("economics", "faithfuld", "seals"))
+    ),
+    
+    # Main panel with outputs
+    mainPanel(
+      h1(textOutput("salutation")), 
+      h3(textOutput("le_texte")),  
+      verbatimTextOutput("summary"),
+      plotOutput("le_graphe")
+    )
+  )
+)
+
